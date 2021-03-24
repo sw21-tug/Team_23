744ea118c6af3ad20d0875b7ac78b2a75868ad93
Florian Schnedlitz <florian.schnedlitz@student.tugraz.at>
2021-03-24 10:40:51 +0100

    OnPoint-000A Add test comment [MG,SF]
    Added test comment to solve issue

--- app/src/main/java/at/tugraz/onpoint/MainActivity.kt
+++ app/src/main/java/at/tugraz/onpoint/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //new test comment solved issue OnPoint-000A
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
