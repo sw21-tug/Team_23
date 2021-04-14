fd99b91597f21c19dd769f667ef3bb77caea545e
csskevin <saigerkevin4@gmail.com>
2021-04-14 09:23:54 +0200

    OnPoint-010A Setup Unit Tests and dependencies. [SF, SK]

---
+++ app/src/androidTest/java/at/tugraz/onpoint/TodoInstrumentedTest.kt
@@ -0,0 +1,20 @@
+package at.tugraz.onpoint
+
+import android.content.ActivityNotFoundException
+import androidx.test.core.app.launchActivity
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TodoInstrumentedTest {
+    @Test
+    fun checkActivitySetup() {
+        try {
+            launchActivity<TodoActivity>()
+        } catch(e: ActivityNotFoundException) {
+            assert(false)
+        }
+    }
+}
