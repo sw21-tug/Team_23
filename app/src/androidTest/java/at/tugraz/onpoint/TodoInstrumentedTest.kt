89e5f27d7aa97fe84d392c93e237767bfe3fba1e
Florian Schnedlitz <florian.schnedlitz@student.tugraz.at>
2021-04-14 11:26:37 +0200

    OnPoint-010A New Test for Layout of ToDo-ListActivity. [SF, SK]

--- app/src/androidTest/java/at/tugraz/onpoint/TodoInstrumentedTest.kt
+++ app/src/androidTest/java/at/tugraz/onpoint/TodoInstrumentedTest.kt
@@ -1,15 +1,28 @@
 package at.tugraz.onpoint
 
 import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import at.tugraz.onpoint.todolist.TodoActivity
+import org.hamcrest.CoreMatchers.not
+import org.junit.Before
+import org.junit.Rule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TodoInstrumentedTest {
+    @Rule
+    @JvmField var activityRule: ActivityTestRule<TodoActivity> = ActivityTestRule(TodoActivity::class.java)
+
     @Test
     fun checkActivitySetup() {
         try {
@@ -17,5 +30,45 @@ class TodoInstrumentedTest {
         } catch(e: ActivityNotFoundException) {
             assert(false)
         }
+    }
+
+    /**
+     * Checks if the initial view setup for the ToDo-Activity is correct
+     */
+    @Test
+    fun checkInitialButtonSetup() {
+        onView(withId(R.id.todo_addButton)).check(matches(isClickable()))
+        onView(withId(R.id.todo_addButton)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.todo_InputField)).check(matches(not(isDisplayed())))
+
+        onView(withId(R.id.todo_saveButton)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.todo_saveButton)).check(matches(not(isClickable())))
+    }
+
+    /**
+     * Check if input fields are correctly displayed after pressing the add button
+     */
+    @Test
+    fun clickAddButton() {
+        onView(withId(R.id.todo_addButton)).perform(click())
+        onView(withId(R.id.todo_InputField)).check(matches(isDisplayed()))
+        onView(withId(R.id.todo_saveButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.todo_addButton)).check(matches(not(isDisplayed())))
+    }
+
+    /**
+     * Check if input fields are correctly displayed after pressing the save button
+     */
+    @Test
+    fun clickSaveButton() {
+        onView(withId(R.id.todo_addButton)).perform(click())
+        onView(withId(R.id.todo_InputField)).check(matches(isDisplayed()))
+        onView(withId(R.id.todo_saveButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.todo_addButton)).check(matches(not(isDisplayed())))
+
+        //press save button
+        onView(withId(R.id.todo_saveButton)).perform(click())
+        checkInitialButtonSetup()
     }
 }
