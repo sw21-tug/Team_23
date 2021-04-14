4f6a26333868cf897e187c0b69259c793eebf371
Matjaž Guštin <dev@matjaz.it>
2021-04-14 11:28:01 +0200

    OnPoint-004 Add test checking for existance of assign. list [GM,MG]
    - Removed hello world textview
    - Added espresso as a project dependency

--- app/src/androidTest/java/at/tugraz/onpoint/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/at/tugraz/onpoint/ExampleInstrumentedTest.kt
@@ -1,25 +1,28 @@
 package at.tugraz.onpoint
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class AssignmentsListInstrumentedTest {
+    @get:Rule
+    var activityRule: ActivityScenarioRule<AssignmentsListActivity> =
+        ActivityScenarioRule(AssignmentsListActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("at.tugraz.onpoint", appContext.packageName)
-        assertEquals(1, 1)
+    fun assignmentsListExists() {
+        launchActivity<AssignmentsListActivity>()
+        onView(withId(R.id.assignmentsList)).check(matches(isDisplayed()))
     }
 }
