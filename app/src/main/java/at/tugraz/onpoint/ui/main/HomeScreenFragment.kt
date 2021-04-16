4d446c102477dc42be1dcde685f0c1e64680130a
Christina Dsubanko <dsubanko@student.tugraz.at>
2021-04-16 20:23:14 +0200

    OnPoint-009A added list to todo and recent [CD, JJ]

--- app/src/main/java/at/tugraz/onpoint/ui/main/HomeScreenFragment.kt
+++ app/src/main/java/at/tugraz/onpoint/ui/main/HomeScreenFragment.kt
@@ -1,10 +1,13 @@
 package at.tugraz.onpoint.ui.main
 
 import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -32,21 +35,48 @@ class HomeScreenFragment : Fragment() {
     ): View? {
         println("begin of fragment")
         val root = inflater.inflate(R.layout.fragment_home_screen, container, false)
-        val textView: TextView = root.findViewById(R.id.section_label)
 
 
-        val todoLayout = root.findViewById(R.id.todo_linear_layout) as ViewGroup
+        val todoLayout = root.findViewById(R.id.homescreen_todo_list_id) as ViewGroup
         val recentLayout = root.findViewById(R.id.recent_linear_layout) as ViewGroup
         val todoList = listOf<String>("todo1", "todo2")
         val recentList = listOf<String>("recent1")
         for(todoItem in todoList){
             // TextView2
             println("In todo loop")
+            val lptv = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT)
+
+            lptv.setMargins(0, 11, 7, 0)
             val textView = TextView(activity)
-            textView.setTextSize(1, 20.0F)
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20.0F)
+            textView.setLayoutParams(lptv)
             textView.setText(todoItem)
+
             textView.setTextColor(Color.MAGENTA)
+
             todoLayout.addView(textView)
+
+
+        }
+
+        for(todoItem in recentList){
+            // TextView2
+            println("In todo loop")
+            val lptv = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT)
+
+            lptv.setMargins(0, 11, 7, 0)
+            val textView = TextView(activity)
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20.0F)
+            textView.setLayoutParams(lptv)
+            textView.setText(todoItem)
+
+            textView.setTextColor(Color.MAGENTA)
+
+            recentLayout.addView(textView)
+
+
         }
 
         return root
