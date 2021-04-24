b827b204e30ea3ed2071d2c3328b4207045a94c0
Christina Dsubanko <dsubanko@student.tugraz.at>
2021-04-24 16:11:41 +0200

    OnPoint-009A clean up and fixed tests [CD, GM]

--- app/src/androidTest/java/at/tugraz/onpoint/HomeScreenTest.kt
+++ app/src/androidTest/java/at/tugraz/onpoint/HomeScreenTest.kt
@@ -1,12 +1,20 @@
 package at.tugraz.onpoint
 
-import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.launchActivity
+import android.content.res.Resources
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.GradientDrawable
+import android.view.View
+import android.widget.EditText
+import androidx.core.content.res.ResourcesCompat
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.internal.util.Checks
 import androidx.test.rule.ActivityTestRule
+import org.hamcrest.Description
+import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,6 +22,54 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class HomeScreenTest{
+
+
+    //source functional item: https://stackoverflow.com/questions/28742495/testing-background-color-espresso-android
+    fun matchesBackgroundColor(expectedResourceId: Int): Matcher<View?>? {
+        return object : BoundedMatcher<View?, View>(View::class.java) {
+            var actualColor = 0
+            var expectedColor = 0
+            var message: String? = null
+            override fun matchesSafely(item: View): Boolean {
+                if (item.getBackground() == null) {
+                    message = item.getId().toString() + " does not have a background"
+                    return false
+                }
+                val resources: Resources = item.getContext().getResources()
+                expectedColor = ResourcesCompat.getColor(resources, expectedResourceId, null)
+                actualColor = try {
+                    (item.getBackground() as ColorDrawable).color
+                } catch (e: Exception) {
+                    (item.getBackground() as GradientDrawable).color!!.defaultColor
+                } finally {
+                }
+                return actualColor == expectedColor
+            }
+
+            override fun describeTo(description: Description) {
+                if (actualColor != 0) {
+                    message = ("Background color did not match: Expected "
+                        + String.format("#%06X", 0xFFFFFF and expectedColor) + " was " + String.format("#%06X", 0xFFFFFF and actualColor))
+                }
+                description.appendText(message)
+            }
+        }
+    }
+/*
+    fun withTextColor(color: Int): Matcher<View?>? {
+        Checks.checkNotNull(color)
+        return object : BoundedMatcher<View?, EditText>(EditText::class.java) {
+            override fun matchesSafely(warning: EditText): Boolean {
+                return color == warning.currentTextColor
+            }
+
+            override fun describeTo(description: Description) {
+                description.appendText("with text color: ")
+            }
+        }
+    }*/
+
+    //source end
 
     @Rule
     @JvmField var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
@@ -40,14 +96,14 @@ class HomeScreenTest{
 
     @Test
     fun background_color(){
-        onView(withId(R.id.mainScreenLayout)).check(matches(hasBackground(R.color.darkGray_main)))
+        onView(withId(R.id.todo_linear_layout)).check(matches(matchesBackgroundColor(R.color.darkGray_main)))
     }
 
     @Test
     fun item_design(){
-        onView(withId(R.id.homescreen_recent_heading_id)).check(matches(hasTextColor(R.color.text_grey)))
-        onView(withId(R.id.homescreen_todo_heading_id)).check(matches(hasTextColor(R.color.text_grey)))
 
+        onView(withId(R.id.homescreen_recent_heading_id)).check(matches(hasTextColor(R.color.black)))
+        onView(withId(R.id.homescreen_todo_heading_id)).check(matches(hasTextColor(R.color.black)))
 
     }
 }
--- app/src/main/java/at/tugraz/onpoint/MainActivity.kt
+++ app/src/main/java/at/tugraz/onpoint/MainActivity.kt
@@ -3,8 +3,6 @@ package at.tugraz.onpoint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import at.tugraz.onpoint.ui.main.HomeScreenFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
